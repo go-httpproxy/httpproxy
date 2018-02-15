dd3eea7db0d6e06f98cda81d9a76607e1dde5f19
Orkun karaduman <orkunkaraduman@gmail.com>
2018-02-15 16:26:08 +0300

    changed demo

--- demo/main.go
+++ demo/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-httpproxy/httpproxy"
 )
 
-func OnError(ctx *httpproxy.Context, when string,
+var logErr = log.New(os.Stderr, "ERR: ", log.LstdFlags)
+
+func OnError(ctx *httpproxy.Context, where string,
 	err *httpproxy.Error, opErr error) {
 	// Log errors.
-	log.Printf("ERR: %s: %s [%s]", when, err, opErr)
+	logErr.Printf("%s: %s [%s]", where, err, opErr)
 }
 
 func OnAccept(ctx *httpproxy.Context, w http.ResponseWriter,
@@ -23,7 +31,7 @@ func OnAccept(ctx *httpproxy.Context, w http.ResponseWriter,
 	return false
 }
 
-func OnAuth(ctx *httpproxy.Context, user string, pass string) bool {
+func OnAuth(ctx *httpproxy.Context, authType string, user string, pass string) bool {
 	// Auth test user.
 	if user == "test" && pass == "1234" {
 		return true
@@ -51,10 +59,16 @@ func OnResponse(ctx *httpproxy.Context, req *http.Request,
 }
 
 func main() {
+	log.SetOutput(os.Stdout)
+	log.Print("started")
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt, os.Kill, syscall.SIGTERM)
+
 	// Create a new proxy with default certificate pair.
 	prx, _ := httpproxy.NewProxy()
 
-	// Set handlers.
+	// Set proxy handlers.
 	prx.OnError = OnError
 	prx.OnAccept = OnAccept
 	prx.OnAuth = OnAuth
@@ -62,6 +76,36 @@ func main() {
 	prx.OnRequest = OnRequest
 	prx.OnResponse = OnResponse
 
-	// Listen...
-	http.ListenAndServe(":8080", prx)
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      prx,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+	}
+	listenErrChan := make(chan error)
+	go func() {
+		listenErrChan <- server.ListenAndServe()
+	}()
+	log.Printf("Listening HTTP %s", server.Addr)
+
+mainloop:
+	for {
+		select {
+		case <-sigChan:
+			break mainloop
+		case listenErr := <-listenErrChan:
+			if listenErr != nil && listenErr == http.ErrServerClosed {
+				break mainloop
+			}
+			log.Fatal(listenErr)
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	server.SetKeepAlivesEnabled(false)
+	if err := server.Shutdown(ctx); err == context.DeadlineExceeded {
+		log.Print("force shutdown")
+	} else {
+		log.Print("graceful shutdown")
+	}
 }
